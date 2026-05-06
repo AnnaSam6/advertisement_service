f1e5726d7a9809e8331b6dac24801772a23caf0a
AnnaSam6 <anna.vitaeva@inbox.ru>
2026-05-06 14:47:24 +0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -6,18 +6,20 @@ DATABASE_URL = "sqlite:///./advertisements.db"
 database = databases.Database(DATABASE_URL)
 metadata = sqlalchemy.MetaData()
 
+# Описание таблицы в БД
 advertisements = sqlalchemy.Table(
     "advertisements",
     metadata,
     sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True, autoincrement=True),
-    sqlalchemy.Column("title", sqlalchemy.String(100), nullable=False),
-    sqlalchemy.Column("description", sqlalchemy.String(1000), nullable=False),
+    sqlalchemy.Column("title", sqlalchemy.String(length=100), nullable=False),
+    sqlalchemy.Column("description", sqlalchemy.String(length=1000), nullable=False),
     sqlalchemy.Column("price", sqlalchemy.Float, nullable=False),
-    sqlalchemy.Column("author", sqlalchemy.String(100), nullable=False),
+    sqlalchemy.Column("author", sqlalchemy.String(length=100), nullable=False),
     sqlalchemy.Column("created_at", sqlalchemy.DateTime, nullable=False),
 )
 
 engine = sqlalchemy.create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
+    DATABASE_URL, 
+    connect_args={"check_same_thread": False}
 )
 metadata.create_all(engine)
